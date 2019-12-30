c6deadc02aeabeba715a14b0b2cdf1472b79c288
zerookrash <zerookrash@me.com>
2019-12-30 16:33:05 -0600

    Agregando los actores a las peliculas que coresponden

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+
+import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
+
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class PeliculaDetalle extends StatelessWidget {
   
@@ -19,6 +23,10 @@ class PeliculaDetalle extends StatelessWidget {
               SizedBox(height: 10.0),
               _posterTitulo( context, pelicula ),
               _descripcion( pelicula ),
+              _descripcion( pelicula ),
+              _descripcion( pelicula ),
+              _descripcion( pelicula ),
+              _crearCasting( pelicula )
             ])
           )
         ],
@@ -106,5 +114,71 @@ class PeliculaDetalle extends StatelessWidget {
 
 
   }
+
+
+  Widget _crearCasting( Pelicula pelicual ) {
+
+    final peliProvider = new PeliculasProvider();
+
+    return FutureBuilder(
+      future: peliProvider.getCast(pelicual.id.toString()),
+      builder: (context, AsyncSnapshot<List> snapshot) {
+        
+        if (snapshot.hasData){
+          return _crearActoresPageView(snapshot.data);
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+      },
+    );
+
+  }
+
+
+Widget _crearActoresPageView( List<Actor> actores ) {
+
+
+  return SizedBox(
+    height: 200.0,
+    child: PageView.builder(
+      pageSnapping: false,
+      controller: PageController(
+        viewportFraction: 0.3,
+        initialPage: 1
+      ),
+      itemCount: actores.length,
+      itemBuilder: ( context, i ) => _actorTarjeta( actores[i] ),
+    ),
+  );
+}
+
+Widget _actorTarjeta( Actor actor ) {
+
+
+  return Container(
+    child: Column(
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: FadeInImage(
+            image: NetworkImage( actor.getFoto() ),
+            placeholder: AssetImage('assets/img/no-image.jpg'),
+            height: 150.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Text(
+          actor.name,
+          overflow: TextOverflow.ellipsis,
+        )
+      ],
+    ),
+  );
+
+
+}
 
 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/models/actores_model.dart';
 
 class PeliculasProvider {
 
@@ -90,5 +91,20 @@ class PeliculasProvider {
 
   }
 
+  Future<List<Actor>> getCast( String peliId ) async {
+
+    final url = Uri.https(_url, '3/movie/$peliId/credits', {
+      'api_key'   : _apikey,
+      'language'  : _language
+    });
+
+    final resp = await http.get(url);
+    final decodedData = json.decode( resp.body );
+
+    final cast = new Cast.fromJsonList(decodedData['cast']);
+
+    return cast.actores;
+
+  }
 
 }
